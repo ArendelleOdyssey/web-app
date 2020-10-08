a95f5e345c576f3e66fa3ccf25e3ebb058a6de17
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-10-08 10:26:04 +0200

    Change condition - Should fix #2

--- preload.js
+++ preload.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // It does not make sense to use the custom titlebar on macOS where
   // it only tries to simulate what we get with the normal behavior anyway.
-  if (!isMacintosh) {
+  if (process.platform != 'darwin') {
 
     // add a menu
     const menu = new Menu();
